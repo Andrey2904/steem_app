7f2c50d532e4a9fd64f81c84ad49db8966b5d949
Andrew Svyazov <svyazov.andrei@yandex.ru>
2025-02-15 20:40:50 +0300

    Completed project

--- stem_play.py
+++ stem_play.py
@@ -19,7 +19,7 @@ def download_settings(url, save_path):
 
 def apply_registry_settings(file_path):
     """
-    Применяет настройки из .reg файла в реестр Windows.
+    Применение настройки из .reg файла в реестр Windows.
     :param file_path: Путь к файлу .reg.
     """
     try:
@@ -30,11 +30,11 @@ def apply_registry_settings(file_path):
 
 def find_steam_path():
     """
-    Пытается найти путь к Steam в реестре Windows.
+    Поиск пути к Steam в реестре Windows.
     :return: Путь к Steam.exe или None, если не найден.
     """
     try:
-        # Открываем ключ реестра, где хранится путь к Steam
+        
         reg_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\\WOW6432Node\\Valve\\Steam")
         steam_path = winreg.QueryValueEx(reg_key, "InstallPath")[0]
         steam_exe = os.path.join(steam_path, "Steam.exe")
@@ -46,7 +46,8 @@ def find_steam_path():
 
 def launch_steam_game(app_id):
     """
-    Запускает игру в Steam по её AppID.
+    Запуск игры в Steam по её AppID.
+    AppID для игры "Goose Goose Duck" = 1568590
     :param app_id: AppID игры в Steam.
     """
     steam_path = find_steam_path()
@@ -56,26 +57,26 @@ def launch_steam_game(app_id):
         return
 
     try:
-        # Запуск игры через Steam
+        
         subprocess.run([steam_path, "-applaunch", str(app_id)], check=True)
         print(f"Игра с AppID {app_id} успешно запущена.")
     except subprocess.CalledProcessError as e:
         print(f"Ошибка при запуске игры: {e}")
 
 if __name__ == "__main__":
-    # Ссылка для скачивания файла настроек
+
     url = "https://drive.google.com/uc?export=download&id=18Yr6wfSAJZTqhttMFVDNx7pZkez2vJBq"
     
-    # Текущая директория для сохранения файла настроек
+    
     current_dir = os.getcwd()
     settings_file = os.path.join(current_dir, "settings.reg")
     
-    # Загрузка и применение настроек
+
     download_settings(url, settings_file)
     apply_registry_settings(settings_file)
     
-    # AppID игры (например, Goose Goose Duck)
-    game_app_id = "1568590"  # Замените на AppID нужной игры
     
-    # Запуск игры
+    game_app_id = "1568590"  
+    
+    
     launch_steam_game(game_app_id)
